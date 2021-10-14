37d4af3afa1894a2110233bdfec25bc28dc50fe8
Ashutosh <ashutripathi733@gmail.com>
2021-10-14 19:07:39 +0530

    finish nav component

---
+++ src/Components/App/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Nav from '../Nav/Nav'
+import './App.css'
+class App extends React.Component {
+    render() {
+        return (
+            <div className="app">
+                {/*Nav section*/}
+                <Nav />
+            </div>
+        )
+    }
+}
+export default App
