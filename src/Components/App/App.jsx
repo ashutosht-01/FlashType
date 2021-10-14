07262e532b00de19118687d1badc4c8268a7e952
Ashutosh <ashutripathi733@gmail.com>
2021-10-14 21:36:44 +0530

    finished landing component

--- src/Components/App/App.jsx
+++ src/Components/App/App.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import Nav from '../Nav/Nav'
+import Landing from '../Landing/Landing'
 import './App.css'
+import landing from '../Landing/Landing'
 class App extends React.Component {
     render() {
         return (
             <div className="app">
                 {/*Nav section*/}
                 <Nav />
+                {/*Landing Section */}
+                <Landing />
             </div>
         )
     }
